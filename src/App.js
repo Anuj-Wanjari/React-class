c37c097df553a9d3c12f5a85505d7a6fbc2da21f
Anuj-Wanjari <anujwanjari5@gmail.com>
2024-06-26 11:33:03 +0530

    cart

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import './App.css';
-import { a, b } from './components/Products/Products';
 import Products from './components/Products/Products';
 import { useState } from 'react';
 import CartContext from './context/CartContext';
+import Cart from './components/Cart/Cart';
+
 function App() {
-  // state variable
-  // inc
-  // dec
   let [cart, setCart] = useState({});
+
   function increaseQuantity(product) {
     const newCart = { ...cart };
-    // if(cart[product.id])
     if (!newCart[product.id]) {
       newCart[product.id] = {
         ...product,
@@ -18,7 +16,6 @@ function App() {
       };
     }
     newCart[product.id].quantity += 1;
-    console.log(newCart);
     setCart(newCart);
   }
 
@@ -32,43 +29,14 @@ function App() {
     setCart(newCart);
   }
 
-  console.log(a, b);
   return (
-    <CartContext.Provider value={{ cart, increaseQuantity, decreaseQuantity}}>
+    <CartContext.Provider value={{ cart, increaseQuantity, decreaseQuantity }}>
       <div className="App">
-        <Products  />
+        <Products />
+        <Cart />
       </div>
     </CartContext.Provider>
   );
 }
 
 export default App;
-
-// {1:{id: 1, quantity: 11}, 2:{id: 2, quantity: 10}, 3:{id: 3, quantity: 10}, 4:{id: 4, quantity: 10}}
-
-// button
-// - 1 +
-
-// let a = {b:10, c:20};
-//a.b = 30;
-// let b = {...a};
-
-// a => 1234
-// a => 12
-
-// {cart: cart, increaseQuantity: increaseQuantity}
-
-
-// Global Parent
-// Global state
-// way to manipulate the state
-// way to consume the state
-
-
-//  Provider
-//  Store
-    // State
-    // Reducer
-// Action
-// Dispatcher
-// Selectors
